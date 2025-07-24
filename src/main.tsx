e8fd6b6802f938c2579d5406bd211cffe8cca971
LotteFiber <ratchanonnine9@gmail.com>
2025-07-24 17:10:12 +0700

    feat: add Redux Toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,22 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { Toaster } from 'sonner';
 
+import { Provider } from 'react-redux';
+
 import { AppRouter } from './routes/router.tsx';
 import { ThemeProvider } from './components/theme-provider.tsx';
 import { AuthProvider } from './contexts/AuthContext';
+import { store } from './app/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <AppRouter />
-      </ThemeProvider>
-      <Toaster />
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <AppRouter />
+        </ThemeProvider>
+        <Toaster />
+      </AuthProvider>
+    </Provider>
   </StrictMode>,
 );
