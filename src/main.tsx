944f55f151ffea4022d6bef7c7e4eae1184de3d2
LotteFiber <ratchanonnine9@gmail.com>
2025-07-24 17:21:54 +0700

    feat: add Query caching: Reac Query (for fetching & caching)

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,39 @@
 import { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
-
 import './index.css';
 import { Toaster } from 'sonner';
-
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { AppRouter } from './routes/router.tsx';
 import { ThemeProvider } from './components/theme-provider.tsx';
 import { AuthProvider } from './contexts/AuthContext';
 import { store } from './app/store.ts';
 
+const queryClient = new QueryClient();
+
+function AppProviders({ children }: { children: React.ReactNode }) {
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            {children}
+          </ThemeProvider>
+        </AuthProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Provider>
+  );
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <AuthProvider>
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <AppRouter />
-        </ThemeProvider>
-        <Toaster />
-      </AuthProvider>
-    </Provider>
+    <AppProviders>
+      <AppRouter />
+      <Toaster />
+    </AppProviders>
   </StrictMode>,
 );
